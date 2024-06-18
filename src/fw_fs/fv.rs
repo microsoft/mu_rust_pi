6c0b4054c9c701a9c435b90f5844f722adad343c
John Schock <joschock@microsoft.com>
2024-06-18 09:01:45 -0700

    Switch FV header to static ref from raw pointer

--- src/fw_fs/fv.rs
+++ src/fw_fs/fv.rs
@@ -80,7 +80,7 @@ pub(crate) struct ExtHeader {
 
 #[derive(Copy, Clone)]
 pub struct FirmwareVolume {
-  fv_header: *const self::Header,
+  fv_header: &'static Header,
 }
 
 impl FirmwareVolume {
@@ -142,11 +142,11 @@ impl FirmwareVolume {
       Err(efi::Status::INVALID_PARAMETER)?;
     }
 
-    Ok(FirmwareVolume { fv_header })
+    Ok(FirmwareVolume { fv_header: fv_header.as_ref().unwrap() })
   }
 
   fn ext_header(&self) -> Option<*const ExtHeader> {
-    let ext_header_offset = unsafe { (*self.fv_header).ext_header_offset as u64 };
+    let ext_header_offset = self.fv_header.ext_header_offset as u64;
     if ext_header_offset == 0 {
       return None;
     }
@@ -154,7 +154,7 @@ impl FirmwareVolume {
   }
 
   fn block_map(&self) -> &[BlockMapEntry] {
-    let block_map_start = unsafe { &(*self.fv_header).block_map }.as_ptr();
+    let block_map_start = self.fv_header.block_map.as_ptr();
     let mut count = 0;
     let mut current_block_map_ptr = block_map_start;
 
@@ -176,7 +176,7 @@ impl FirmwareVolume {
   }
 
   pub fn first_ffs_file(&self) -> FfsFile {
-    let mut ffs_address = self.fv_header as u64;
+    let mut ffs_address = self.fv_header as *const Header as u64;
     if let Some(ext_header) = self.ext_header() {
       // if ext header exists, then file starts after ext header
       unsafe {
@@ -185,7 +185,7 @@ impl FirmwareVolume {
       }
     } else {
       // otherwise the file starts after the main header.
-      unsafe { ffs_address += (*self.fv_header).header_length as u64 }
+      ffs_address += self.fv_header.header_length as u64
     }
     ffs_address = align_up(ffs_address, 0x8);
     // Note: it appears possible from the EDK2 implementation that an FV could have a file system with no actual
@@ -198,16 +198,16 @@ impl FirmwareVolume {
     FfsFileIterator::new(self.first_ffs_file())
   }
 
-  pub fn base_address(&self) -> u64 {
-    self.fv_header as u64
+  pub fn base_address(&self) -> efi::PhysicalAddress {
+    self.fv_header as *const Header as efi::PhysicalAddress
   }
 
-  pub fn top_address(&self) -> u64 {
-    unsafe { self.base_address() + (*self.fv_header).fv_length }
+  pub fn top_address(&self) -> efi::PhysicalAddress {
+    self.base_address() + self.fv_header.fv_length
   }
 
   pub fn attributes(&self) -> EfiFvbAttributes2 {
-    unsafe { (*self.fv_header).attributes }
+    self.fv_header.attributes
   }
 
   pub fn get_lba_info(&self, lba: u32) -> Result<(u32, u32, u32), ()> {
